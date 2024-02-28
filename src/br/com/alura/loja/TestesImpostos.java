2a188535cbc893fd483bc1b9d33b707b26a0a3a6
Tales Aguiar <talesdd@outlook.com>
2024-02-28 11:04:46 -0300

    adding strategy and CoR classes

---
+++ src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,14 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+
+import java.math.BigDecimal;
+
+public class TestesImpostos {
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"),1);
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
+    }
+}
